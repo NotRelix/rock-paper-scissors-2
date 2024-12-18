def6d6dd1f9d7b39a5918b41685f971eb07bd076
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-18 21:15:32 +0800

    feat: randomize rock paper scissors options

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('test')
+function getRandomNumber() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+function getComputerChoice() {
+    const randomNumber = getRandomNumber();
+    let choice = '';
+    if (randomNumber === 1) {
+        choice = 'rock';
+    } else if (randomNumber === 2) {
+        choice = 'paper';
+    } else {
+        choice = 'scissors';
+    }
+    return choice;
+}
