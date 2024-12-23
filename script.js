bb16fe6f118cc645f32727ab60cb6f2cca18d503
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-23 16:25:21 +0800

    feat: limit game to 5 points

--- script.js
+++ script.js
@@ -30,32 +30,25 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     let winner = ''
     if (humanChoice === computerChoice) {
-        console.log(`It's a tie! Player(${humanChoice}) vs Computer(${computerChoice})`)
         winner = 'tie'
     } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-        console.log(`Computer won! Player(${humanChoice}) vs Computer(${computerChoice})`)
         winner = 'computer';
     } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        console.log(`Player won! Player(${humanChoice}) vs Computer(${computerChoice})`)
         winner = 'human';
     } else if (humanChoice === 'paper' && computerChoice == 'rock') {
-        console.log(`Player won! Player(${humanChoice}) vs Computer(${computerChoice})`)
         winner = 'human';
     } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        console.log(`Computer won! Player(${humanChoice}) vs Computer(${computerChoice})`)
         winner = 'computer';
     } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        console.log(`Computer won! Player(${humanChoice}) vs Computer(${computerChoice})`)
         winner = 'computer';
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        console.log(`Player won! Player(${humanChoice}) vs Computer(${computerChoice})`)
         winner = 'human';
     }
     return winner;
 }
 
-function printScore(humanScore, computerScore) {
-    console.log(`Player(${humanScore}) vs Computer(${computerScore})`)
+function changeScore() {
+    score.textContent = `You: ${humanScore} vs Computer: ${computerScore}`
 }
 
 function playGame(e) {
@@ -63,20 +56,30 @@ function playGame(e) {
     const computerSelection = getComputerChoice()
     let winner = playRound(humanSelection, computerSelection);
 
-    if (winner === 'human') {
-        humanScore++;
-        display.textContent = `You won! ${humanSelection} vs ${computerSelection}`
-    } else if (winner === 'computer') {
-        computerScore++;
-        display.textContent = `You lost! ${humanSelection} vs ${computerSelection}`
-    } else {
-        display.textContent = `Tie! ${humanSelection} vs ${computerSelection}`
+    if (humanScore < 5 && computerScore < 5) {
+        if (winner === 'human') {
+            humanScore++;
+            display.textContent = `You won! ${humanSelection} vs ${computerSelection}`
+        } else if (winner === 'computer') {
+            computerScore++;
+            display.textContent = `You lost! ${humanSelection} vs ${computerSelection}`
+        } else {
+            display.textContent = `Tie! ${humanSelection} vs ${computerSelection}`
+        }
     }
 
-    printScore(humanScore, computerScore)
+    if (humanScore >= 5) {
+        overallWinner.textContent = `Congratulations, you win! do you want to play again?`
+    } else if (computerScore >= 5) {
+        overallWinner.textContent = `Better luck next time! do you want to play again?`
+    }
+
+    changeScore()
 }
 
 const display = document.querySelector('.display');
+const score = document.querySelector('.score')
+const overallWinner = document.querySelector('.overall-winner')
 
 let humanScore = 0;
 let computerScore = 0;
